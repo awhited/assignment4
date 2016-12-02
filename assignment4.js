98fd1d32db274cd3779a174b9ed9159f5514e448
awhited <auw6@pitt.edu>
2016-12-01 20:47:55 -0500

    swag

--- assignment4.js
+++ assignment4.js
@@ -9,9 +9,32 @@
 // $.show(), $.hide(), $.slideup(), $.slidedown(), $.fadein(), $.fadeout()
 // Add content from requests with something like
 // $.html(), $.text(), etc.
-// keyup events could be helpful to get value of field as the user types
+// // keyup events could be helpful to get value of field as the user types
+
+var addr = "http://www.mattbowytz.com/simple_api.json?data=all";
 
 (function() {
   // Magic!
-  console.log('Keepin\'n it clean with an external script!');
-})();
+  // This is honsestly the coolest code bit I've written in this class. 
+  //
+  $.getJSON(addr, function(stuff) { 
+    if (stuff.status == 200) {
+      console.log("yay!");
+      $('.flexsearch-input').on("keyup", function(text) {
+        $('.results').empty();
+        var input = $(this).val().toLowerCase();
+        $.each(stuff.data, function(index, value) {
+          $.each(value, function(index, string) {
+            if (string.toLowerCase().startsWith(input)) {
+              $('.results').append("<a target='_blank' href='http://www.google.com/#q=" + 
+                string + "'>" + string + "</a></br>");
+            };  
+          });
+        });
+      });
+    } else {
+      console.log("Boo you stink!");
+    };
+  }); 
+})(window, document, jQuery);
+
